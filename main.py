d2188259acc2191def3b4b7eb7e57c7cded6dabc
Kein <fardz9@bk.ru>
2023-02-12 00:58:26 +0300

    test dataclases and types from typing

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from dataclasses import dataclass
+from typing import *
 
+@dataclass
+class DbConfig:
+    pswd : Optional[int]
+    name : Union[str, int]
+
+
+db = DbConfig(
+    pswd=23424234234,
+    name="dbBot"
+    )
+print(db.name)
